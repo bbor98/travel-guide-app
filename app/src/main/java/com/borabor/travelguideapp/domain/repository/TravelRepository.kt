ba9548b931e9475b0594b4175bbb3e501fbac1b1
Bora Bor <bbor98@gmail.com>
2022-10-03 05:45:03 +0300

    Create travel repository interface

---
+++ app/src/main/java/com/borabor/travelguideapp/domain/repository/TravelRepository.kt
@@ -0,0 +1,10 @@
+package com.borabor.travelguideapp.domain.repository
+
+import com.borabor.travelguideapp.domain.model.Category
+import com.borabor.travelguideapp.domain.model.Travel
+import com.borabor.travelguideapp.util.Resource
+
+interface TravelRepository {
+    suspend fun getTravelList(): Resource<List<Travel>>
+    suspend fun getGuideCategories(): Resource<List<Category>>
+}
