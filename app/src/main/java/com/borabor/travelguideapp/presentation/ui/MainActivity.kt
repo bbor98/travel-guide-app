15904fcacc0db287b895556a175109a7829218a0
Bora Bor <bbor98@gmail.com>
2022-10-04 06:15:53 +0300

    Setup BottomNavigationView and NavHost

---
+++ app/src/main/java/com/borabor/travelguideapp/presentation/ui/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.borabor.travelguideapp.presentation.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.borabor.travelguideapp.R
+import com.borabor.travelguideapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
+    }
+}
