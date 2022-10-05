234a2091643e243e1e96b49444f85b401fd8354a
Bora Bor <bbor98@gmail.com>
2022-10-05 03:42:29 +0300

    Remove redundant object in Resource class

--- app/src/main/java/com/borabor/travelguideapp/util/Resource.kt
+++ app/src/main/java/com/borabor/travelguideapp/util/Resource.kt
@@ -3,5 +3,4 @@ package com.borabor.travelguideapp.util
 sealed class Resource<out T> {
     data class Success<out T>(val data: T) : Resource<T>()
     data class Error(val message: String) : Resource<Nothing>()
-    object Loading : Resource<Nothing>()
 }
