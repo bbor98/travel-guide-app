146f814ba9dc1419be890ad4b959b582820f7df5
Bora Bor <bbor98@gmail.com>
2022-10-07 07:05:58 +0300

    Remove DTOs and DTO mapping functions

--- app/src/main/java/com/borabor/travelguideapp/data/remote/api/TravelApi.kt
+++ app/src/main/java/com/borabor/travelguideapp/data/remote/api/TravelApi.kt
@@ -1,20 +1,20 @@
 package com.borabor.travelguideapp.data.remote.api
 
-import com.borabor.travelguideapp.data.remote.dto.CategoryDTO
-import com.borabor.travelguideapp.data.remote.dto.TravelDTO
+import com.borabor.travelguideapp.domain.model.Category
+import com.borabor.travelguideapp.domain.model.Travel
 import retrofit2.http.*
 
 interface TravelApi {
     @GET("travel_list")
-    suspend fun getTravelList(): List<TravelDTO>
+    suspend fun getTravelList(): List<Travel>
 
     @GET("guide_categories")
-    suspend fun getGuideCategories(): List<CategoryDTO>
+    suspend fun getGuideCategories(): List<Category>
 
     @FormUrlEncoded
     @PUT("travel_list/{id}")
     suspend fun updateBookmark(
         @Path("id") id: String,
         @Field("isBookmark") isBookmark: Boolean
-    ): TravelDTO
+    ): Travel
 }
--- app/src/main/java/com/borabor/travelguideapp/data/repository/TravelRepositoryImpl.kt
+++ app/src/main/java/com/borabor/travelguideapp/data/repository/TravelRepositoryImpl.kt
@@ -1,7 +1,5 @@
 package com.borabor.travelguideapp.data.repository
 
-import com.borabor.travelguideapp.data.mapper.toCategory
-import com.borabor.travelguideapp.data.mapper.toTravel
 import com.borabor.travelguideapp.data.remote.api.TravelApi
 import com.borabor.travelguideapp.domain.repository.TravelRepository
 import com.borabor.travelguideapp.util.SafeApiCall
@@ -14,14 +12,14 @@ class TravelRepositoryImpl @Inject constructor(
     private val safeApiCall: SafeApiCall
 ) : TravelRepository {
     override suspend fun getTravelList() = safeApiCall.execute {
-        api.getTravelList().map { it.toTravel() }
+        api.getTravelList()
     }
 
     override suspend fun getGuideCategories() = safeApiCall.execute {
-        api.getGuideCategories().map { it.toCategory() }
+        api.getGuideCategories()
     }
 
     override suspend fun updateBookmark(id: String, isBookmark: Boolean) = safeApiCall.execute {
-        api.updateBookmark(id, isBookmark).toTravel()
+        api.updateBookmark(id, isBookmark)
     }
 }
